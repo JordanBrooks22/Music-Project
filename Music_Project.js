25baada70c27479acf930a99786943bbff0e1dae
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-11-18 22:48:19 -0500

    added functions to my js file

---
+++ Music_Project.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function musicRequest(){
+     $.ajax({
+            url: "http://www.devcodecampmusiclibrary.com/api/music", 
+            type: "GET",
+            dataType: "json",
+             success: function(result){
+                 dataDisplay(result);
+      
+        }});
+}
+musicRequest();
+
+function dataDisplay(data){
+    console.log(data);
+    for(let i = 0; i < data.length; i++){
+        console.log(data[i].title)
+        $("#musicBody").append(
+           " <tr> " +
+                   " <td>"+data[i].title+"</td>" + 
+                   " <td>"+data[i].album+"</td>" + 
+                   " <td>"+data[i].genre+"</td>" + 
+                   " <td>"+data[i].releaseDate+"</td>" + 
+                " </tr>"
+        );
+    }
+}
